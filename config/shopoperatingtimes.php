a6d8a8f2ce1abf0f1fa4ae465425157801efb1f9
Nick Taylor <nicktaylor611@gmail.com>
2018-05-28 20:42:38 +0100

    created configuration file to store our default listing of opening times

---
+++ config/shopoperatingtimes.php
@@ -0,0 +1,75 @@
+<?php
+return [
+    'Monday' => [
+        [
+            'open' => '09:00',
+            'closed' => '12:20',
+            'openedResponseMessage' => 'Open',
+            'closedResponseMessage' => 'Closed for Lunch'
+        ],
+        [
+            'open' => '12:20',
+            'closed' => '17:00',
+            'openedResponseMessage' => 'Open',
+            'closedResponseMessage' => 'Closed for the day'
+        ]
+    ],
+    'Tuesday' => [
+        [
+            'open' => '09:00',
+            'closed' => '12:20',
+            'openedResponseMessage' => 'Open',
+            'closedResponseMessage' => 'Closed for Lunch'
+        ],
+        [
+            'open' => '12:20',
+            'closed' => '17:00',
+            'openedResponseMessage' => 'Open',
+            'closedResponseMessage' => 'Closed for the day'
+        ]
+    ],
+    'Wednesday' => [
+        [
+            'open' => '09:00',
+            'closed' => '12:20',
+            'openedResponseMessage' => 'Open',
+            'closedResponseMessage' => 'Closed for Lunch'
+        ],
+        [
+            'open' => '12:20',
+            'closed' => '17:00',
+            'openedResponseMessage' => 'Open',
+            'closedResponseMessage' => 'Closed for the day'
+        ]
+    ],
+    'Thursday' => [
+        [
+            'open' => '09:00',
+            'closed' => '12:20',
+            'openedResponseMessage' => 'Open',
+            'closedResponseMessage' => 'Closed for Lunch'
+        ],
+        [
+            'open' => '12:20',
+            'closed' => '17:00',
+            'openedResponseMessage' => 'Open',
+            'closedResponseMessage' => 'Closed for the day'
+        ]
+    ],
+    'Friday' => [
+        [
+            'open' => '09:00',
+            'closed' => '12:20',
+            'openedResponseMessage' => 'Open',
+            'closedResponseMessage' => 'Closed for Lunch'
+        ],
+        [
+            'open' => '12:20',
+            'closed' => '17:00',
+            'openedResponseMessage' => 'Open',
+            'closedResponseMessage' => 'Closed for the day'
+        ]
+    ],
+    'Saturday' => [],
+    'Sunday' => []
+];
