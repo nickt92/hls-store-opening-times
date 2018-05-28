2986bc17c5354295b5bae76d95ad748719aaf5b1
Nick Taylor <nicktaylor611@gmail.com>
2018-05-28 20:44:10 +0100

    default configuration for store closures

---
+++ config/shopclosures.php
@@ -0,0 +1,50 @@
+<?php
+return [
+    [
+        'description' => 'New Years Day',
+        'start' => '01-01-2017',
+        'end' => '01-01-2017',
+        'reoccurring' => true,
+        'enabled' => true
+    ],
+
+    [
+        'description' => 'Good Friday',
+        'start' => '14-04-2017',
+        'end' => '14-04-2017',
+        'reoccurring' => true,
+        'enabled' => true
+    ],
+
+    [
+        'description' => 'Spring Bank Holiday',
+        'start' => '01-05-2017',
+        'end' => '01-05-2017',
+        'reoccurring' => true,
+        'enabled' => true
+    ],
+
+    [
+        'description' => 'Christmas Day',
+        'start' => '25-12-2017',
+        'end' => '25-12-2017',
+        'reoccurring' => true,
+        'enabled' => true
+    ],
+
+    [
+        'description' => 'Christmas Holidays',
+        'start' => '20-12-2017',
+        'end' => '30-12-2017',
+        'reoccurring' => true,
+        'enabled' => true
+    ],
+
+    [
+        'description' => 'New Years Eve',
+        'start' => '31-12-2017',
+        'end' => '31-12-2017',
+        'reoccurring' => true,
+        'enabled' => true
+    ]
+];
