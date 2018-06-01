1e90638c6f8cba0c2a6ccc7c4451153e9eb2ba34
Nick Taylor <nicktaylor611@gmail.com>
2018-06-01 01:44:33 +0100

    added model method to get store opening times to be reused within controller

--- app/ShopOperatingTime.php
+++ app/ShopOperatingTime.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DateTime;
 
 class ShopOperatingTime extends Model
 {
@@ -14,6 +15,30 @@ class ShopOperatingTime extends Model
     protected $fillable = [
         'opening_time', 'closing_time', 'closed_message'
     ];
+
+    /**
+     * Get Stores opening times
+     *
+     * @param DateTime $dt
+     * @return object
+     */
+    public static function getStoresOpeningTimes(DateTime $dt = null)
+    {
+        if (!$dt instanceof DateTime) {
+            $dt = new DateTime('now');
+        }
+
+        $currentDayInt = $dt->format('N');
+        $currentTime = $dt->format('H:i:s');
+
+        $weekdayOpeningTimesCollection = ShopOperatingTime::where([
+            ['shop_operating_weekday_id', $currentDayInt],
+            ['opening_time', '<=', $currentTime],
+            ['closing_time', '>=', $currentTime]
+        ])->get();
+
+        return $weekdayOpeningTimesCollection;
+    }
 
     /**
      * Set belongs-to relationship for operating times -> weekdays
