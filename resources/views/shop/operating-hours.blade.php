33b14baf2e149702f945e2c5e6ec76dacd1cbf4e
Nick Taylor <nicktaylor611@gmail.com>
2018-06-01 01:46:54 +0100

    touchups + added closures

--- resources/views/shop/operating-hours.blade.php
+++ resources/views/shop/operating-hours.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{__('HLR - Shop')}}</title>
+        <title>{{__('HLS - Shop')}}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -75,15 +75,36 @@
         <div class="flex-center position-ref full-height">
             <div class="content">
                 <div class="title m-b-md">
-                    {{__('HLR Demo Shop')}}
+                    {{__('HLS Demo Shop')}}
                 </div>
                 <div class="sub-title">
                     {{__('Opening Times')}}:
                 </div>
+                @if ($errors->any()) 
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{$error}}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
+                <p>
+                    {{__('Current Shop Status')}}: {{$isShopOpen ? __('Open') : __('Closed')}}
+                </p>
+                @if ($isShopOpen)
+                    <p>
+                        {{__('Shop will next be closed')}} : 
+                    </p>
+                @else
+                    <p>
+                        {{__('Shop will next be open')}} :
+                    </p>
+                @endif
                 <table>
                     <thead>
                         <tr>
-                            <thead></thead>
+                            <th>Store Hours</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -107,6 +128,32 @@
                                 </tr>
                             @endif
                         @endforeach
+                    </tbody>
+                </table>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Store Closures</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @if (!empty($allUpcomingClosures))
+                            @foreach ($allUpcomingClosures as $id => $closureInfo)
+                                <tr>
+                                    <td>
+                                        {{__('Closed for')}} {{$closureInfo['description']}} :
+                                        {{ Carbon\Carbon::parse($closureInfo['start'])->format('l jS F') }}
+                                        @if ($closureInfo['start'] !== $closureInfo['finish'])
+                                            - {{ Carbon\Carbon::parse($closureInfo['finish'])->format('l jS F') }}
+                                        @endif
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <tr>
+                                <td></td>
+                            </tr>
+                        @endif
                     </tbody>
                 </table>
             </div>
